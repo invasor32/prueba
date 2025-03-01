6f428092308813f67c5b67453deb5a85886e0cba
Luis Brito <30274509+invasor32@users.noreply.github.com>
2025-03-01 03:07:57 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,99 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-terreno-imagen-360",
+      "name": "terreno imagen 360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.07562501295538304,
+        "pitch": -0.03322218315783232,
+        "fov": 1.2836595229737267
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.16528696763401733,
+          "pitch": -0.30426393866297857,
+          "title": "592",
+          "text": "Precio:<div><br></div><div>$ 40.000.000 Al contado</div>"
+        },
+        {
+          "yaw": 0.08082381275365805,
+          "pitch": -0.35694714360866925,
+          "title": "591",
+          "text": "Precio:&nbsp;<div><br></div><div>$ 36.000.000</div>"
+        },
+        {
+          "yaw": -0.38570329760193914,
+          "pitch": -0.2932985691557161,
+          "title": "593",
+          "text": "Precio:<div><br></div><div>$ 40.000.000</div>"
+        },
+        {
+          "yaw": -0.55785439717376477,
+          "pitch": -0.25550798729156504,
+          "title": "596",
+          "text": "Precio:<div><br></div><div>$ 36.000.000</div>"
+        },
+        {
+          "yaw": -0.7516973676820943,
+          "pitch": -0.3363979238973477,
+          "title": "597",
+          "text": "Precio:<div><br></div><div>$ 40.000.000</div>"
+        },
+        {
+          "yaw": -0.7020803801153139,
+          "pitch": -0.017231168271441035,
+          "title": "598",
+          "text": "<div>Precio:</div><div><br></div>$ 40.000.000"
+        },
+        {
+          "yaw": -0.49372934796084593,
+          "pitch": 0.21613229696884915,
+          "title": "569",
+          "text": "Precio:<div><br></div><div>40.000.000</div>"
+        },
+        {
+          "yaw": -0.31621228993042465,
+          "pitch": 0.1970698572447681,
+          "title": "570",
+          "text": "Precio:<div><br></div><div>$ 36.000.000</div><div>$ 30.000.000 Contado</div>"
+        },
+        {
+          "yaw": -0.11300633589193481,
+          "pitch": 0.15068022935789202,
+          "title": "571",
+          "text": "Precio:<div><br></div><div>Desde, $25.000.000</div>"
+        },
+        {
+          "yaw": 0.10969035694216217,
+          "pitch": 0.08411818597667938,
+          "title": "572",
+          "text": "Precio:<div><br></div><div>$ 30.000.000</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Terreno",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
